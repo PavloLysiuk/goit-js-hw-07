63939976e777bcddd744b94504bcdb8d984c2bf6
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-06-21 23:58:31 +0300

    task 7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-//* Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
+// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 
 // Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
 
@@ -18,19 +18,18 @@ import { galleryItems } from './gallery-items.js';
 const galleryUl = document.querySelector('.gallery');
 
 function createMarkupItem(arr) {
-  return arr
-    .map(
-      ({ preview, original, description }) =>
-        `<li class="gallery__item">
-          <a class="gallery__link" href="${original}">
-            <img class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}" />
-          </a>
-        </li>`
-    )
-    .join('');
+  return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `).join('');
 }
 
 galleryUl.insertAdjacentHTML('beforeend', createMarkupItem(galleryItems));
@@ -38,13 +37,19 @@ galleryUl.insertAdjacentHTML('beforeend', createMarkupItem(galleryItems));
 galleryUl.addEventListener('click', onclick);
 
 function onclick(e) {
-  e.preventDefault()
+  e.preventDefault();
   const cardItem = e.target.closest('.gallery__item');
+
   if (cardItem) {
-    const originalImageUrl = cardItem.querySelector('.gallery__image').dataset.source;
-    const instance = basicLightbox.create(`
-      <img src="${originalImageUrl}">
-      `);
-      instance.show()
+    const original = cardItem.querySelector('.gallery__image').dataset.source;
+    const instance = basicLightbox.create(`<img src="${original}">`);
+    instance.show();
+  }
+}
+
+function onEscKeyDown(e) {
+  console.log(e);
+  if (e.code === 'Escape') {
+    instance.close();
   }
 }
