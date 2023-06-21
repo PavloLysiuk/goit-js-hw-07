6f7f13ea8e70ac33813be0810efa01b4caf34695
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-06-21 23:25:41 +0300

    task 1 in progress

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,50 @@
+//* Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
+
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// 2. Реалізація делегування на ul.gallery і отримання url великого зображення.
+// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
+
+// Закриття з клавіатури
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryUl = document.querySelector('.gallery');
 
-const galleryUl = document.querySelector('.gallery')
+function createMarkupItem(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+          </a>
+        </li>`
+    )
+    .join('');
+}
+
+galleryUl.insertAdjacentHTML('beforeend', createMarkupItem(galleryItems));
 
 galleryUl.addEventListener('click', onclick);
 
+function onclick(e) {
+  e.preventDefault()
+  const cardItem = e.target.closest('.gallery__item');
+  if (cardItem) {
+    const originalImageUrl = cardItem.querySelector('.gallery__image').dataset.source;
+    const instance = basicLightbox.create(`
+      <img src="${originalImageUrl}">
+      `);
+      instance.show()
+  }
+}
