653bf52af1216eda98bf4332e370dac319c869f9
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-06-24 17:56:07 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,9 +3,9 @@
 // Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
 
 // 1 Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
-//* 2 Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
-//* 3 Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
-//* 4 Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+// 2 Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+// 3 Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// 4 Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
 
 import { galleryItems } from './gallery-items.js';
 
@@ -13,18 +13,16 @@ const galleryUl = document.querySelector('.gallery');
 
 galleryUl.insertAdjacentHTML('beforeend', createMarkupItem(galleryItems));
 
-galleryUl.addEventListener('click', onClick);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 function createMarkupItem(arr) {
   return arr.map(({ preview, original, description }) => `
     <li class="gallery__item">
       <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
+        <img class="gallery__image" src="${preview}" alt="${description}" />
       </a>
     </li>
     `).join('');
